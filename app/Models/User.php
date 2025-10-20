fa8050fa34bb66e1e5ba51f5cfb6594c10fb34ab
wahyu93 <wahyu.wahyudi1005@gmail.com>
2025-10-20 21:28:02 +0800

    authentication - instal fortify

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,6 +38,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_secret',
+        'two_factor_recovery_codes',
+        'two_factor_confirmed_at',
     ];
 
     /**
